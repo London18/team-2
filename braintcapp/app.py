d50587a22d7b0ebc0a125c959d128ed83bcf192d
cyy2427 <cyy2427@yeah.net>
2018-11-09 23:19:49 +0000

    added db

--- braintcapp/app.py
+++ braintcapp/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import sqlite3
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    con = sqlite3.connect('../db/knowledge_base.db')
+    c = con.cursor()
+    c.execute("SELECT * FROM knowledge")
+    knowledge = c.fetchone()
+    print(c.fetchone())
+    return "knowledge "+knowledge[1]
 
 
 if __name__ == '__main__':

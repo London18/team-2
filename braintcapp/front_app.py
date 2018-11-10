b4b11e115a060876e964787a60695fcf8b0589e4
FrankWJW <541489980@qq.com>
2018-11-10 08:55:24 +0000

    first commit brain ct website

--- braintcapp/front_app.py
+++ braintcapp/front_app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+import sqlite3
+import numpy as np
 import os
 from flask import abort, redirect, url_for
 
@@ -36,7 +38,6 @@ def do_admin_login():
 def hello():
     form = ReusableForm(request.form)
 
-
     print (form.errors)
     if request.method == 'POST':
         name=request.form['name']
@@ -49,13 +50,43 @@ def hello():
             flash('All the form fields are required. ')
 
         if name == "sympton":
-            return render_template('home.html')
+            return render_template('symptom.html')
 
 
     return render_template('hello.html', form=form)
 
+@app.route('/sympton', methods=['GET','POST'])
+def getlinks():
+    con = sqlite3.connect('../db/knowledge_base.db')
+    c = con.cursor()
+    # sql2 = ""
+    sql = "SELECT * FROM knowledge where category_id = (select category_id from category where category_name = 'symptons')"
+    c.execute(sql)
+    k3 = c.fetchall()
+    render_string = '<ul>'
+
+    for i in k3:
+        k2 = np.array(i)
+
+        render_string += '<li><a href="'+k2[1]+'">' + k2[1] + '</ a></li>'
+
+    render_string += '</ul>'
+
+    return render_string
 
 
+
+# @app.route("/base", methods=['GET', 'POST'])
+# def base():
+#     return render_template('base.html')
+#
+# @app.route("/base/register", methods=['GET', 'POST'])
+# def register():
+#     return render_template('register.html')
+#
+# @app.route("/base/login", methods=['GET', 'POST'])
+# def login():
+#     return render_template('login.html')
 
 if __name__ == "__main__":
     # app.secret_key = os.urandom(12)
